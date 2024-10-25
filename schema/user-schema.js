f5734493a66ad9241cfe1f2ccdc8abb3b23ccc76
ibraheem <ibraheem45622@gmail.com>
2024-10-25 21:30:28 +0500

    fourth commit

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt'; 
+import bcrypt from 'bcryptjs'; // Change this to bcryptjs
 
 const userSchema = mongoose.Schema({
     firstname: String,
@@ -19,12 +19,25 @@ const userSchema = mongoose.Schema({
       type: Date,
       default: Date.now
     }
-})
+});
 
-userSchema.methods.comparePassword = function (password) {
-    return bcrypt.compare(password, this.password);
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next(); // Only hash the password if it has been modified
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate salt
+        this.password = await bcrypt.hash(this.password, salt); // Hash the password
+        next(); // Proceed to save the user
+    } catch (error) {
+        next(error); // Pass any errors to the next middleware
+    }
+});
+
+// Method to compare the provided password with the stored hashed password
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password); // Compare with bcryptjs
 };
 
-const user = mongoose.model('user', userSchema)
+const User = mongoose.model('User', userSchema); // Capitalized model name for convention
 
-export default user;
+export default User;
